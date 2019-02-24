19600b1c03f8145b45fbed5a03d05c586d6c7203
Kyle Miracle <kyle.miracle@nvc-inc.net>
2019-02-24 10:13:09 -0500

    init commit

---
+++ src/ui-component.ts
@@ -0,0 +1,49 @@
+export interface Component {
+    fields: Array<any>;
+    source: any;
+}
+
+export interface FieldArgs {
+    label: string;
+    transform: (val: any) => string;
+}
+
+export function source(name: string) {
+    return function(constructor: any) {
+        constructor.prototype._source = name;
+    }
+}
+
+export function field({ label, transform }: FieldArgs) {
+    return function(target: Object, propName: string) {
+        Object.defineProperty(target, `${propName}-UIField`, {
+            get() {
+                return ({
+                    label,
+                    type: 'field',
+                    value: transform(this[propName])
+                });
+            },
+            enumerable: true
+        });
+    }
+}
+
+export default class UIComponent {
+    renderComponent() {
+        const component: Component = {
+            fields: [],
+            source: '',
+        };
+
+        for (let k in this) {
+            if (k.endsWith("UIField")) {
+                component.fields.push(this[k]);
+            }
+            if (k === '_source') {
+                component.source = this[k];
+            }
+        }
+        return JSON.stringify(component);
+    }
+}
