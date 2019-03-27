1ddcb014d14ad84507fcbd87acd3561b4c570764
Kyle Miracle <kmiracle86@gmail.com>
2019-03-26 21:37:01 -0400

    WIP - add table component

--- test/ui-component.test.ts
+++ test/ui-component.test.ts
@@ -1,5 +1,13 @@
 import {} from 'ts-jest'
-import UIComponent, { field, source, FieldArgs, Component } from '../src/ui-component'
+import UIComponent, {
+  field,
+  source,
+  table,
+  FieldArgs,
+  TableArgs,
+  SortOrder,
+  Component
+} from '../src/ui-component'
 
 describe('UIComponent', () => {
   it('UIComponent is instantiable', () => {
@@ -25,6 +33,27 @@ describe('UIComponent', () => {
     expect(testField).toHaveProperty('label')
     expect(testField).toHaveProperty('type')
     expect(testField).toHaveProperty('value')
+  })
+
+  it('table() creates the table component', () => {
+    const target: any = {
+      testProp: [
+        { col2: '3', col1: 'col1', col3: 'c' },
+        { col2: '4', col1: 'col1', col3: 'b' },
+        { col2: '1', col1: 'col1', col3: 'e' },
+        { col2: '2', col1: 'col1', col3: 'a' },
+        { col2: '5', col1: 'col1', col3: 'd' }
+      ]
+    }
+    const prop = 'testProp'
+    const args: TableArgs = {
+      title: 'Table Title',
+      columns: ['col3', 'col2', 'col1'],
+      sortingColumn: 'col3',
+      sortOrder: SortOrder.DESC
+    }
+    table(args)(target, prop)
+    const testTable: Object = target['testProp-UITable']
   })
 
   it('transform() is called', () => {
@@ -83,6 +112,39 @@ describe('UIComponent', () => {
     expect((json as any).fields[0].label).toEqual('label')
     expect((json as any).fields[0].type).toEqual('field')
     expect((json as any).fields[0].value).toEqual('foo-changed')
+  })
+
+  it('UIComponent.renderComponent() should add UITables', () => {
+    @source('Telegeography')
+    class TestComponent extends UIComponent {
+      @table({
+        title: 'Table Title',
+        columns: ['col3', 'col2', 'col1'],
+        sortingColumn: 'col2',
+        sortOrder: SortOrder.DESC
+      })
+      foo: Array<any>
+
+      constructor(foo: Array<any>) {
+        super()
+        this.foo = foo
+      }
+    }
+    const rows = [
+      { col2: '3', col1: 'col1', col3: 'c' },
+      { col2: '4', col1: 'col1', col3: 'b' },
+      { col2: '1', col1: 'col1', col3: 'e' },
+      { col2: '2', col1: 'col1', col3: 'a' },
+      { col2: '5', col1: 'col1', col3: 'd' }
+    ]
+    const testComponent = new TestComponent(rows)
+    const json: Object = testComponent.renderComponent()
+    expect(json).toHaveProperty('tables')
+    expect((json as any).tables[0]).toHaveProperty('title')
+    expect((json as any).tables[0]).toHaveProperty('columns')
+    expect((json as any).tables[0]).toHaveProperty('type')
+    expect((json as any).tables[0]).toHaveProperty('value')
+    expect((json as any).tables[0].value[0]).toEqual(['d', '5', 'col1'])
   })
 
   it('UIComponent.renderComponent() should add source field', () => {
